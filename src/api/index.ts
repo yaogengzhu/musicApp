ad5098f3367a152c0004620d933648fab359f304
yaogengzhu <455947455@qq.com>
2021-03-22 23:57:37 +0800

    updat

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,23 +2,23 @@
 import Taro from '@tarojs/taro'
 
 class Fetch {
-  post(option) {
-    return new Promise((resolve, reject) => {
-      Taro.request({
-        url: 'https://music-eta.vercel.app/' + option.url,
-        data: Object.assign(option.params || {}, {
-        }),
-        header: {
-          'content-type': 'application/json' // 默认值
-        },
-        method: option.mehtods || 'POST'
-      }).then(res => {
-        return resolve(res)
-      }).catch(err => {
-        return reject(err)
-      })
-    })
-  }
+    post(option) {
+        return new Promise((resolve,reject) => {
+            Taro.request({
+                url: 'http://yaogeng.top' + option.url,
+                data: Object.assign(option.params || {},{
+                }),
+                header: {
+                    'content-type': 'application/json' // 默认值
+                },
+                method: option.mehtods || 'POST'
+            }).then(res => {
+                return resolve(res)
+            }).catch(err => {
+                return reject(err)
+            })
+        })
+    }
 }
 
 const fetch = new Fetch()
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -8,42 +8,46 @@ import './index.scss'
 
 
 export default class Index extends Component {
-  constructor() {
-    super(...arguments)
-    this.state = {
-      list: []
-    }
-  }
-
-  componentDidMount() {
-    fetch.post({
-      url: '/top/album'
-    }).then(res => {
-      console.log(res)
-    })
-  }
-
-  render() {
-    return (
-      <View className='index'>
-        {
-          this.state.list.map(item => (
-            <AtCard
-              extraStyle={{ marginBottom: '10px' }}
-              key={item.id}
-              note='笑话'
-              extra={item.ct}
-              title={item.title}
-              thumb={item.img}
-            >
-              <Image src={item.img} className='img'></Image>
-              <View>{item.title}</View>
-            </AtCard>
-          ))
+    constructor() {
+        super(...arguments)
+        this.state = {
+            list: []
         }
+    }
 
-        <TabBar />
-      </View>
-    )
-  }
+    componentDidMount() {
+        fetch.post({
+            url: '/login/cellphone',
+            params: {
+                phone: '15871602731',
+                pasword: 'hx19940608..'
+            }
+        }).then(res => {
+            console.log(res)
+        })
+    }
+
+    render() {
+        return (
+            <View className='index'>
+                {
+                    this.state.list.map(item => (
+                        <AtCard
+                            extraStyle={{ marginBottom: '10px' }}
+                            key={item.id}
+                            note='笑话'
+                            extra={item.ct}
+                            title={item.title}
+                            thumb={item.img}
+                        >
+                            <Image src={item.img} className='img'></Image>
+                            <View>{item.title}</View>
+                        </AtCard>
+                    ))
+                }
+
+                <TabBar />
+            </View>
+        )
+    }
 }
